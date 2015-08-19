9fdc5cbfbb8992092f15fcd4ebf1332837aeab1b
Devon Taylor <devon@komlite.com>
2015-08-19 13:16:39 -0400

    Changed styling
    Deleted commented code, begin htmlclean implementation (syntax for code
    view) and complete reset for email design view

--- js/script.js
+++ js/script.js
@@ -1,192 +1,38 @@
-/*
-$(document).ready(function(e) {
-	
-	
-	var bannerWRAP = $.ajax({type: "GET", url: "partials/banner.html", async: false}).responseText;
-	
-	var introWRAP = $.ajax({type: "GET", url: "partials/intro.html", async: false}).responseText;
-	
-	var ctaWRAP = $.ajax({type: "GET", url: "partials/cta.html", async: false}).responseText;
-	
-	var feaWRAP = $.ajax({type: "GET", url: "partials/features.html", async: false}).responseText;
-	
-	var blockWRAP = $.ajax({type: "GET", url: "partials/blockwrap.html", async: false}).responseText;
-	
-	
-	//Not working in chrome (localhost http request) temporarily based in vars.js file.
-	
-	var $buildArray = [];
-	
-    $('#form').submit(function( event ) {
-  		
-  		event.preventDefault();
-		
-		$('.inactive').removeClass('inactive');
-		
-		if ($('#form input[type="submit"]').hasClass('errorAnimated')) {
-			$('#form input[type="submit"]').removeClass('errorAnimated');
-		}
-		
-		$('#form input[type="submit"]').addClass('done');
-		
-		$('.controls input[type="submit"]').val("Code Generated");
-		
-		$buildArray = [];
-		
-		//BUILD BANNER OUTPUT
-		$('#build').append(bannerWRAP);
-		$('#build a').attr("href", $('#bannerlink').val());
-		$('#build img').attr("src", $('#bannerpath').val()).attr("title", $('#bannertitle').val()).attr("alt", $('#bannertitle').val());
-		$('#bannerCode').val($('#build').html());
-		$buildArray.push($('#bannerCode').val());
-		$('#build').empty();
-		
-		//BUILD INTRO OUTPUT
-		$('#build').append(introWRAP).append(ctaWRAP);
-		$('#build #myCopy').replaceWith($('#intro').val());
-		
-		$('.myCTA').attr('href', $('#ctaLink').val()); // URL
-		
-		if($('#ctatype').val() == 'cta1') {
-			$('.myCTA img').attr('src', 'http://image.exct.net/lib/fe5e15707d6c007a7410/m/1/shop-now.png').attr('title', 'Shop Now').attr('alt', 'Shop Now');
-		}else if ($('#ctatype').val() == 'cta2'){
-			$('.myCTA img').attr('src', 'http://image.exct.net/lib/fe5e15707d6c007a7410/m/1/buy-now.png').attr('title', 'Buy Now').attr('alt', 'Buy Now');
-		}else if ($('#ctatype').val() == 'cta3'){
-			$('.myCTA img').attr('src', 'http://image.exct.net/lib/fe5e15707d6c007a7410/m/1/learn-more.png').attr('title', 'Buy Now').attr('alt', 'Learn More');
-		}else {
-			$('.myCTA img').attr('src', 'http://image.exct.net/lib/fe5e15707d6c007a7410/m/1/enter-to-win.png').attr('title', 'Buy Now').attr('alt', 'Enter to Win');
-		}
-		
-		$('.myCTA').removeClass('myCTA');
-		$('#introCode').val($('#build').html());
-		$buildArray.push($('#introCode').val());
-		$('#build').empty();
-		
-		//BUILD FEATURES
-		$('#build').append(feaWRAP);
-		$('#build .f1a').attr("href", $('#fea1url').val()).removeClass('f1a');
-		$('#build .f1i').attr("src", $('#fea1path').val()).attr("alt", $('#fea1cap').val()).removeClass('f1i');
-		$('#build .f1c').text($('#fea1cap').val()).removeClass('f1c');
-		
-		$('#build .f2a').attr("href", $('#fea2url').val()).removeClass('f2a');
-		$('#build .f2i').attr("src", $('#fea2path').val()).attr("alt", $('#fea2cap').val()).removeClass('f2i');;
-		$('#build .f2c').text($('#fea2cap').val()).removeClass('f2c');
-		
-		$('#build .f3a').attr("href", $('#fea3url').val()).removeClass('f3a');
-		$('#build .f3i').attr("src", $('#fea3path').val()).attr("alt", $('#fea3cap').val()).removeClass('f3i');
-		$('#build .f3c').text($('#fea3cap').val()).removeClass('f3c');
-		$('#feaCode').val($('#build').html());
-		$buildArray.push($('#feaCode').val());
-		$('#build').empty();
-		
-		//BUILD FIRST TWO BLOCKS
-		$('#build').append(blockWRAP);
-		$('#build .block1a').attr("href", $('#block1url').val()).removeClass('block1a');
-		$('#build .block1i').attr("src", $('#block1path').val()).attr("alt", $('#block1cap').val()).removeClass('block1i');
-		$('#build .block1c').text($('#block1cap').val()).removeClass('block2c');
-		
-		$('#build .block2a').attr("href", $('#block2url').val()).removeClass('block2a');
-		$('#build .block2i').attr("src", $('#block2path').val()).attr("alt", $('#block2cap').val()).removeClass('block2i');
-		$('#build .block2c').text($('#block2cap').val()).removeClass('block2c');
-		
-		$('#block1Code').val($('#build').html());
-		$buildArray.push($('#block1Code').val());
-		$('#build').empty();
-		
-		//BUILD SECOND TWO BLOCKS
-		$('#build').append(blockWRAP);
-		$('#build .block1a').attr("href", $('#block3url').val()).removeClass('block1a');
-		$('#build .block1i').attr("src", $('#block3path').val()).attr("alt", $('#block3cap').val()).removeClass('block1i');
-		$('#build .block1c').text($('#block3cap').val()).removeClass('block2c');
-		
-		$('#build .block2a').attr("href", $('#block4url').val()).removeClass('block2a');
-		$('#build .block2i').attr("src", $('#block4path').val()).attr("alt", $('#block4cap').val()).removeClass('block2i');
-		$('#build .block2c').text($('#block4cap').val()).removeClass('block2c');
-		
-		$('#block2Code').val($('#build').html());
-		$buildArray.push($('#block2Code').val());
-		$('#build').empty();
-	});
-	
-	$('#formContainer').addClass('active');
-	
-	$('#previewButton').click(function(event){
-	
-		event.preventDefault();
-		
-		if ($buildArray.length == 0) {
-			$('#form input[type="submit"]').addClass('errorAnimated');
-		}else{
-			$('.active').fadeOut(0);
-			$('.active').removeClass('active');
-			$('#preview').fadeIn(0);
-			$('#preview').addClass('active');
-			$('#preview>.container').append($buildArray);
-		}
+var $iframeView = $('#emailPreviewFrame');
 
-	});
-	
-	$('#sourceButton').click(function(event){
-	
-		event.preventDefault();
-		
-		if ($buildArray.length == 0) {
-			$('#form input[type="submit"]').addClass('errorAnimated');
-		}else{
-			$('.active').fadeOut(0);
-			$('.active').removeClass('active');
-			$('#source').fadeIn(0);
-			$('#source').addClass('active');
-			$('#source>.container').text($buildArray).append();
-		}
-	
-	});
-	
-	$('#clearButton').click(function(event){
-	
-		event.preventDefault();
-		
-		if ($buildArray.length == 0) {
-			$('#form input[type="submit"]').addClass('errorAnimated');
-		}else{
-			$buildArray.length = [];
-			$('textarea').val("");
-			$('#form input[type="submit"]').removeClass('done');
-			$('.controls input[type="submit"]').val("Generate Code");
-			$('.button').addClass('inactive');
-		}
-	
-	});
-	
-	$('.exit').click(function(event){
-	
-		event.preventDefault();
-		
-		$('.active').fadeOut(0);
-		$('.active').removeClass('active');
-		$('.container').empty();
-		
-		$('#formContainer').fadeIn(0);
-		$('#formContainer').addClass('active');
-	
-	});	
+$iframeView.ready(function(){
+	$iframeView.contents().find("body").html($("<div></div>").append('<h2>This was appended!</h2>').html());
+	//$iframeView.contents().find('body').append('<h2>THis was appended</h2>');
+	console.log($iframeView.contents())
+	console.log($iframeView.contents().find('body'));
+	console.log($iframeView.contents().find('body').contents());
+	console.log($iframeView.contents().html('<h1></h1>'));
+	console.log($iframeView.contents().find("body").html($("<div></div>")))
+	console.log($iframeView.find('#appender'));
+	//$iframeView.contents().find('body').html().prepend('<h2>THis was appended</h2>');
+	$iframeView.contents().find("h1")
 	
 });
-*/
 
 $(window).ready(function(){
 	
 	for (var i = 0; i < $('xmp').length; i++) {
+	
 		var orig = $('xmp:eq(' +[i] +')').clone(true).html();
 		var unes = unescape(orig);
 		var html = $.parseHTML(unes);
+		var formhtml = $.htmlClean(orig, { format: true });
+		
 		$('#viewer').append(html);
+		$('#sourceViewer').append(formhtml);
+		
 	}
 	
 });
+
 $(window).resize(function(){
 	
-	$('#viewer').css({
+	$('.viewWrap').css({
 		'max-height':$(window).innerHeight() - 45, 
 		'overflow':'auto', 
 		'margin-top':'-10px'
@@ -194,7 +40,7 @@ $(window).resize(function(){
 	
 });
 
-$('#viewer').css({
+$('.viewWrap').css({
 	'max-height':$(window).innerHeight() - 45, 
 	'overflow':'auto', 
 	'margin-top':'-10px'
@@ -210,39 +56,27 @@ $('input, textarea, select').focus(function(){
 	}
 });
 
-/*
-$('input[type="text"], textarea, select, xmp').change(function(){
-	
-	$('#viewer').empty();
-
-	for (var i = 0; i < $('xmp').length; i++) {
-		var orig = $('xmp:eq(' +[i] +')').clone(true).html();
-		var unes = unescape(orig);
-		var html = $.parseHTML(unes);
-		$('#viewer').append(html);
-	}
-	
-});
-*/
 function appendToViewer(){
 
 	$('#viewer').empty();
+	$('#sourceViewer').empty();
+	//emptying old content
 	
 	for (var i = 0; i < $('xmp').length; i++) {
 		var orig = $('xmp:eq(' +[i] +')').clone(true).html();
+		//grab html data from each xmp tag using index
 		var unes = unescape(orig);
-		var html = $.parseHTML(unes);
-		$('#viewer').append(html);
+		var escp = escape(orig)
+		//var html = $.parseHTML(unes);
+		$('#sourceViewer').append(orig);
+		$('#viewer').append(orig);
 	}
 	
 }
 
-$('input[type="text"], textarea, select').on('input change click focus', function(){
-	
-	appendToViewer();
-	
-});
+//Appends input (design and source) on each input / focus / click / change onto an input element using function above
 
+$('input[type="text"], textarea, select').on('input change click focus', function(){appendToViewer();});
 
 $('.source').click(function(event){
 	
@@ -264,6 +98,7 @@ $('.source').click(function(event){
 	}
 
 });
+
 $('.edit').click(function(event){
 	
 	event.preventDefault();
@@ -283,25 +118,22 @@ $('.collapser').click(function(event){
 
 	event.preventDefault();
 	
-	parCla = $(this).parent().parent().parent().parent().attr('class');
+	if ($(this).parent().parent().parent().next().hasClass('collapsed')) {
 	
-	if ($('.build.'+parCla).hasClass('collapsed')) {
-	
-		$('.build.'+parCla).removeClass('collapsed').slideDown();
+		$(this).parent().parent().parent().next().removeClass('collapsed').slideDown();
 		$(this).children().attr('class', 'fa fa-minus');
 		$(this).parent().parent().parent().css('margin-bottom', '0px');
 		
 	}else {
 	
-		$('.build.'+parCla).addClass('collapsed').slideUp();
+		$(this).parent().parent().parent().next().addClass('collapsed').slideUp();
 		$(this).children().attr('class', 'fa fa-plus');
 		$(this).parent().parent().parent().css('margin-bottom', '10px');
 		
 	}
-	
-	console.log(parCla)
 
 });
+
 $('.collapseAll').click(function(event){
 
 	event.preventDefault();
