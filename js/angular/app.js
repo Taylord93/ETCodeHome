136aeac4470f4647b4f9dbc721c8d2d9bae10f55
taylord93 <snkvnm@yahoo.com>
2015-08-25 22:42:31 -0400

    Updated Templating

--- js/angular/app.js
+++ js/angular/app.js
@@ -3,9 +3,11 @@ var app = angular.module('codegen', ['ngRoute']);
 app.config(
 	function($routeProvider) {
 	    $routeProvider.when('/', {
-	        templateUrl : 'file://localhost/Users/DTaylo02/Documents/ETCodeGen/'
-	    }).when('/what', {
-	        templateUrl : 'file://localhost/Users/DTaylo02/Documents/ETCodeGen/partials/blockwrap.html'
+	        templateUrl : 'file:///Users/DevonTaylor/Documents/Web%20Dev/ETCodeHome/partials/layouts/default.html'
+	    }).when('/product-alert', {
+	        templateUrl : 'file:///Users/DevonTaylor/Documents/Web%20Dev/ETCodeHome/partials/layouts/product-alert.html'
+	    }).when('/product-update', {
+	        templateUrl : 'file:///Users/DevonTaylor/Documents/Web%20Dev/ETCodeHome/partials/layouts/product-update.html'
 	    }).otherwise({
 	        redirectTo : '/'
 	    });
